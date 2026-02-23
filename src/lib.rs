7dbf65bb753d1e2a9c8cf7e17efe955326bebb48
FrCrypticCode <102952930+FrCrypticCode@users.noreply.github.com>
2026-02-23 12:12:12 +0100

    Update virtualMem
    Defining in progress, to use this code, you need to comment alloc/desalloc

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,27 @@
 
 #[derive(Clone,Copy)]
 struct IdEntry{
-    id:u16,
-    addr:u16,
+    addr:VirtualAddr,
     frame:u16,
+    ptr:*mut u8
+}
+
+#[derive(Clone,Copy)]
+struct VirtualAddr{
+    id:u16,
+    pos:u16
+}
+impl VirtualAddr{
+    pub fn new(id:u16,pos:u16)->Self{
+        VirtualAddr { id, pos }
+    }
+    pub fn get_pid(&self)->u16{self.id}
+    pub fn get_pos(&self)->u16{self.pos}
+}
+
+struct PhysFrame{
+    frame:IdFrame,
+    ptr:*mut u8
 }
 
 #[derive(Debug)]
@@ -19,6 +37,7 @@ enum DesallocResult{
     DesallocSuccess,
     AlreadyFree
 }
+type IdFrame = usize;   // Réflexion sur le type : usize peut être trop volumineux
 pub struct Allocator<const N:usize,const S:usize,const F:usize>{
     bytes:[u8;N],
     ids:[Option<IdEntry>;S],
@@ -40,9 +59,9 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
     const fn slots(&self)->usize{return self.bitmap.len()}
     const fn frame_size(&self)->usize{return N/F}
     // Physical Allocate
-    pub fn alloc_phys(&mut self,need:usize)->Option<&mut [u8]>{
+    fn alloc_phys(&mut self,need:usize)->Option<PhysFrame>{
         if need == 0 || need>self.slots(){return None}
-        let mut ptr:usize = 0;
+        let ptr:usize;
         if self.offset+need>self.bitmap.len(){
             match self.find_hole(need){
                 Some(p)=>{ptr = p},
@@ -58,7 +77,8 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
             AllocResult::AllocSuccess=>{
                 self.lock(rng);
                 self.offset = core::cmp::max(self.offset,ptr+need);
-                return Some(&mut self.bytes[rngbytes.0..rngbytes.1])
+                return Some(PhysFrame {frame:ptr,ptr:self.bytes[rngbytes.0..rngbytes.1].as_mut_ptr()})
+                //return Some((ptr,self.bytes[rngbytes.0..rngbytes.1].as_mut_ptr()))
             },
             AllocResult::AlreadyAlloc=>{
                 if let Some(p) = self.find_hole(need){
@@ -67,7 +87,7 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
                     if let AllocResult::AllocSuccess = self.verify_alloc(rng){
                         self.lock(rng);
                         self.offset = core::cmp::max(self.offset,p+need);
-                        return Some(&mut self.bytes[rngbytes.0..rngbytes.1])
+                        return Some(PhysFrame {frame:ptr,ptr:self.bytes[rngbytes.0..rngbytes.1].as_mut_ptr()})
                     }
                 }
                 None
@@ -75,7 +95,7 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
             AllocResult::NoMemory=>{None}
         }  
     }
-    fn find_hole(&self,need:usize)->Option<(usize)>{
+    fn find_hole(&self,need:usize)->Option<(IdFrame)>{
         let mut find = (false,0,0);
         for (p,i) in self.bitmap.iter().enumerate(){
             if *i==0 && !find.0{find = (true,p,find.2+1);}
@@ -87,7 +107,7 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
         }
         None
     }
-    fn verify_alloc(&self,rng:(usize,usize))->AllocResult{
+    fn verify_alloc(&self,rng:(IdFrame,IdFrame))->AllocResult{
         for f in self.bitmap[rng.0..rng.1].iter(){
             if *f == 1{
                 return AllocResult::AlreadyAlloc
@@ -95,9 +115,9 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
         }
         return AllocResult::AllocSuccess
     }
-    fn lock(&mut self,rng:(usize,usize)){for d in self.bitmap[rng.0..rng.1].iter_mut(){*d=1}}
+    fn lock(&mut self,rng:(IdFrame,IdFrame)){for d in self.bitmap[rng.0..rng.1].iter_mut(){*d=1}}
     // Physical Desallocate
-    pub fn free_phys(&mut self,slots:&mut [u8])->bool{
+    fn free_phys(&mut self,slots:&mut [u8])->bool{
         let ptr = slots.as_ptr() as usize;
         let start = self.bytes.as_ptr() as usize;
         let start_frame = (ptr - start)/self.frame_size();
@@ -118,7 +138,7 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
             DesallocResult::AlreadyFree=>{false}
         }
     }
-    fn verify_desalloc(&self,rng:(usize,usize))->DesallocResult{
+    fn verify_desalloc(&self,rng:(IdFrame,IdFrame))->DesallocResult{
         for f in self.bitmap[rng.0..rng.1].iter(){
             if *f == 0{
                 return DesallocResult::AlreadyFree
@@ -126,11 +146,48 @@ impl<const N:usize, const S:usize,const F:usize> Allocator<N,S,F>{
         }
         return DesallocResult::DesallocSuccess
     }
-    fn unlock(&mut self,rng:(usize,usize)){for f in self.bitmap[rng.0..rng.1].iter_mut(){*f=0;}}
+    fn unlock(&mut self,rng:(IdFrame,IdFrame)){for f in self.bitmap[rng.0..rng.1].iter_mut(){*f=0;}}
     //
     //Virtualize Part - Not functionnaly
-    pub fn alloc(&mut self,process:u16,need:usize)->AllocResult{
-        AllocResult::AllocSuccess
+    pub fn alloc(&mut self,process:u16,need:usize){ // ->Result<(*mut u8,AllocResult)>
+        let nb_frames:usize;
+        if need%self.frame_size()!=0{nb_frames=need/self.frame_size()+1;}
+        else{nb_frames=need /self.frame_size();}
+        // All frames needed are allocate at the first request => Perfect bloc allocate
+        if let Some(mem) = self.alloc_phys(nb_frames){
+            let new =IdEntry{
+                addr: VirtualAddr::new(mem.frame as u16,0),
+                frame:mem.frame as u16,
+                ptr:mem.ptr
+            };
+            for i in self.ids.iter_mut(){
+                if i.is_none(){*i=Some(new);}
+            }
+        }
+        else{
+            // Frames are splited, need to request multiples times to make a perfect virtual access
+            // get_part_frame !!!
+        }
+        /*
+            struct IdEntry{
+                id:u16,
+                addr:u16,
+                frame:u16,
+                ptr:*mut u8
+            }
+        */
+    }
+    fn get_part_frame(&mut self,size:usize)->Option<PhysFrame>{  //
+        if size == 0{
+            return None
+        }
+        if let Some(f) = self.alloc_phys(size){
+            return Some(f)
+        }
+        else{
+            return self.get_part_frame(size/2)
+        }
+        
     }
     pub fn desalloc(&mut self,process:u16)->DesallocResult{
         DesallocResult::DesallocSuccess
